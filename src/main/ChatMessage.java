6d0fbd8d44d4b27da731de7727b1c22d6c141076
a00795612 <a00795612@SE12L0303-23.ad.bcit.ca>
2014-09-09 14:52:24 -0700

    Test

--- src/main/ChatMessage.java
+++ src/main/ChatMessage.java
@@ -8,7 +8,7 @@ import java.io.*;
  * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no 
  * need to count bytes or to wait for a line feed at the end of the frame
  */
-//ASDFASDFa
+//ASDFASDFa2
 public class ChatMessage implements Serializable {
 
 	protected static final long serialVersionUID = 1112122200L;
