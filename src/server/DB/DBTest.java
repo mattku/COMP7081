e1c614e792492a1acc5f85249a374a680ea4e572
bthomas50 <bthomas50@my.bcit.ca>
2014-09-16 12:03:48 -0700

    Added functionality for adding/removing users

--- src/server/DB/DBTest.java
+++ src/server/DB/DBTest.java
@@ -21,6 +21,12 @@ public class DBTest
         try
         {
             Connection conn = DB.connect();
+            
+            Users.addUser(conn, "testUser", "testPwd", "test");
+            assert(Users.checkPassword(conn, "testUser", "testPwd") == Users.PwdResult.SUCCESS);
+            Users.removeUser(conn, "testUser");
+            assert(Users.checkPassword(conn, "testUser", "testPwd") == Users.PwdResult.NO_SUCH_USER);
+            
             assert(Users.checkPassword(conn, "user", Util.mySQLCompatibleMD5("password")) == Users.PwdResult.SUCCESS);
             assert(Users.checkPassword(conn, "notaname", Util.mySQLCompatibleMD5("password")) == Users.PwdResult.NO_SUCH_USER);
             assert(Users.checkPassword(conn, "user", Util.mySQLCompatibleMD5("notpassword")) == Users.PwdResult.INCORRECT_PASSWORD);
@@ -28,6 +34,8 @@ public class DBTest
             assert(Users.getRole(conn, "user").equals("user"));
             assert(Users.getRole(conn, "admin").equals("admin"));
             assert(Users.getRole(conn, "notaname") == null);
+            
+            
         }
         catch(SQLException sqlE)
         {
